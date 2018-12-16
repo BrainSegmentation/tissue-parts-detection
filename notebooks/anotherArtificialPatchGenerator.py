b7eb60d4ca49efd1cc18fc0ab7cc9a8059b232ba
Thomas Templier <thomas.templier2@gmail.com>
2018-12-16 21:24:35 +0100

    (wip anotherGenerator) templates directly generated from the labelme json

--- notebooks/anotherArtificialPatchGenerator.py
+++ notebooks/anotherArtificialPatchGenerator.py
@@ -18,28 +18,93 @@
 			# locally add the throw
 			# check if collision
 			# fill current enveloppes# Workflow
-			
+
+import json
+import os
+import random
+import numpy as np
+import cv2 as cv
+
 rootFolder = r'C:\Collectome\Students\Docs\RawWafers\BIOPWafers\newWafersBIOP\Wafer_14'		
+jsonPath = r'C:\Collectome\Students\Docs\RawWafers\BIOPWafers\newWafersBIOP\Wafer_14\stitched_BF_Test_small.json'
+
 
 nThrows = 300
 patchSize = np.array([600,600])
-basketSize = 1.2 * patchSize # size of the large picture in which sections are thrown (sections are thrown into the basket). Will be cropped to patchSize after throwing.
+basketSize = (1.4 * patchSize).astype(int) # size of the large picture in which sections are thrown (sections are thrown into the basket). Will be cropped to patchSize after throwing.
+throwSize = (1.2 * patchSize).astype(int) # size of the area in which the sections are thrown
 
 # read templates
-	templates = {}
-	templates['n']['mag/tissue/envelope']['mask/points']
+templates = {}
 
-# populate nOrientations for each template
+with open(jsonPath, 'r') as f:
+	labelmeJson = json.load(f)
+print(labelmeJson.keys())
+imName = labelmeJson['imagePath']
+fluoName = imName.replace('BF_Test', 'DAPI')
+
+imPath = os.path.join(os.path.dirname(jsonPath), imName)
+fluoPath = os.path.join(os.path.dirname(jsonPath), fluoName)
+
+im = cv.imread(imPath)
+
+# templates['n']['mag/tissue/envelope/bbox/centroid']['points/im/mask']
+sections = zip(*(iter(labelmeJson['shapes']),) * 3)
+for id,[t,m,e] in enumerate(sections): # tissue, magnet, envelope
+	templates[id] = {}
+
+	bbox = cv.boundingRect(np.array(e['points'])) # the bounding box of the envelope
+	templates[id]['bbox'] = bbox
+
+	templates[id]['t'] = {}
+	templates[id]['t']['points'] = np.array(t['points']) - np.array([bbox[0], bbox[1]])
 	
+	templates[id]['m'] = {}
+	templates[id]['m']['points'] = np.array(m['points']) - np.array([bbox[0], bbox[1]])
 
+	templates[id]['e'] = {}
+	templates[id]['e']['points'] = np.array(e['points']) - np.array([bbox[0], bbox[1]])
+
+	templates[id]['centroid'] = np.mean(templates[id]['e']['points'].T, axis=1).astype(int)
+
+	eMask = np.zeros((bbox[3], bbox[2]), dtype=np.uint8) # envelope mask
+	cv.fillConvexPoly(eMask, templates[id]['e']['points'], 255)
+	templates[id]['e']['mask'] = eMask
+		
+	tMask = np.zeros((bbox[3], bbox[2]), dtype=np.uint8) # tissue mask
+	cv.fillConvexPoly(tMask, templates[id]['t']['points'], 255)
+	templates[id]['t']['mask'] = tMask
+
+	mMask = np.zeros((bbox[3], bbox[2]), dtype=np.uint8) # magnetic mask
+	cv.fillConvexPoly(mMask, templates[id]['m']['points'], 255)
+	templates[id]['m']['mask'] = mMask
+	
+	imCropped = im[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]]
+	imMasked = cv.bitwise_and(imCropped, imCropped, mask = eMask)
+	templates[id]['e']['im'] = imMasked
+
+	# cv.imshow('image',imMasked)
+	# cv.waitKey(0)
+	# cv.imshow('image',tMask)
+	# cv.waitKey(0)
+	# cv.imshow('image',mMask)
+	# cv.waitKey(0)
+	# cv.imshow('image',eMask)
+	# cv.waitKey(0)
+	# cv.destroyAllWindows()		
+		
+# populate nOrientations for each template
+
+# templates['n']['mag/tissue/envelope/bbox/centroid']['points/im/mask']
 for idThrow in range(nThrows):
-	basket = np.zeros((*basketSize), np.uint8)
+	basket = np.zeros(basketSize, np.uint8)
 	
 	# pick random template with a random orientation
-	template = templates[random.choice(list(a))]
+	template = templates[random.choice(list(templates.keys()))]
 	
 	# randomly throw
-		x,y = 
+	x = random.randint((patchSize[0]-throwSize[0])/2, patchSize[0] - (patchSize[0]-throwSize[0])/2)
+	y = random.randint((patchSize[1]-throwSize[1])/2, patchSize[1] - (patchSize[1]-throwSize[1])/2)
 				
 	# extract the box in basket (the current image keeping track of the thrown envelopes)
 	# locally add the throw
