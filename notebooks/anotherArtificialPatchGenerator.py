9e5897d514afe304306c0c3aeb8c6529e65ac45d
Thomas Templier <thomas.templier2@gmail.com>
2018-12-17 13:43:12 +0100

    anotherDataGenerator is working with rotation of templates

--- notebooks/anotherArtificialPatchGenerator.py
+++ notebooks/anotherArtificialPatchGenerator.py
@@ -1,14 +1,8 @@
-# the current gen_artificial_img.py is working (that's good) but can be optimized/rewritten:
-	# - the templates are not randomly rotated
-	# - the collision boxes look cumbersome, instead we could throw masks and check with the opencv functions whether there is an increase in the number of white pixels equal to the number of white pixels in the mask (if yes, then no collision). These should only involve calls to the efficient functions of opencv that are in C, instead of doing custom checks in python
-	# - no need to generate a large image that then needs to be cropped in subpatches: directly generate a small patch. It will be easier to parallelize
-
-# the user provides
-	# for each section template
-		# brain
-		# mag
-		# envelope
-			# it would typically involve the nearby dummy. The envelope is used for collision checks
+# with labelme the user manually labels the following *in this order*
+	# for each section template (typically 5, ideally with slightly different looks)
+		# tissue polygon
+		# mag polygon
+		# envelope polygon : the envelope typically involves the nearby dummy. The envelope is used for collision checks
 	# a background area free of sections
 
 import json
@@ -25,8 +19,8 @@ artificialFolder = os.path.join(rootFolder, 'artificialData')
 pathlib.Path(artificialFolder).mkdir(exist_ok=True)
 
 nPatches = 100
-nThrows = 1000
-nAngles = 10 # rotations of the templates
+nThrows = 2000
+nAngles = 30 # rotations of the templates
 
 patchSize = np.array([600,600])
 basketSize = (1.4 * patchSize).astype(int) # size of the large picture in which sections are thrown (sections are thrown into the basket). Will be cropped to patchSize after throwing.
@@ -40,77 +34,99 @@ imName = labelmeJson['imagePath']
 fluoName = imName.replace('BF_Test', 'DAPI')
 imPath = os.path.join(rootFolder, imName)
 fluoPath = os.path.join(rootFolder, fluoName)
+
 ######################################
-# populate templates: points, masks, im, envelope area, 
+# populate templates: points, masks, im, envelope area, etc. with different angles
 ######################################
 templates = {}
-im = cv.imread(imPath)
-imFluo = cv.imread(fluoPath,0)
+im = cv.imread(imPath, 0)
+imFluo = cv.imread(fluoPath, 0)
+w,h = im.shape
+angles = np.linspace(start=0, stop=360, num=nAngles)[:-1]
+counter = 0
 for id,[t,m,e] in enumerate(sections): # tissue, magnet, envelope
-	templates[id] = {}
-	
-	centroid = np.mean(np.array(e['points']).T, axis = 1)
-	
-	bbox = cv.boundingRect(np.array(e['points'])) # the bounding box of the envelope
-	templates[id]['bbox'] = bbox
-
-	templates[id]['t'] = {}
-	templates[id]['t']['points'] = np.array(t['points']) - np.array([bbox[0], bbox[1]])
-	
-	templates[id]['m'] = {}
-	templates[id]['m']['points'] = np.array(m['points']) - np.array([bbox[0], bbox[1]])
-
-	templates[id]['e'] = {}
-	templates[id]['e']['points'] = np.array(e['points']) - np.array([bbox[0], bbox[1]])
-
-	eMask = np.zeros((bbox[3], bbox[2]), dtype=np.uint8) # envelope mask
-	cv.fillConvexPoly(eMask, templates[id]['e']['points'], 255)
-	templates[id]['e']['mask'] = eMask
+	for angle in angles:
+		# get a larger bbox around the template to make rotations
+		bbox = cv.boundingRect(np.array(e['points'])) # the bounding box of the envelope
+		largerBbox = np.array([bbox[0]-bbox[2], bbox[1]-bbox[3], 3*bbox[2], 3*bbox[3]])
 		
-	tMask = np.zeros((bbox[3], bbox[2]), dtype=np.uint8) # tissue mask
-	cv.fillConvexPoly(tMask, templates[id]['t']['points'], 255)
-	templates[id]['t']['mask'] = tMask
-
-	mMask = np.zeros((bbox[3], bbox[2]), dtype=np.uint8) # magnetic mask
-	cv.fillConvexPoly(mMask, templates[id]['m']['points'], 255)
-	templates[id]['m']['mask'] = mMask
-	
-	imCropped = im[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]]
-	imMasked = cv.bitwise_and(imCropped, imCropped, mask = eMask)
-	templates[id]['e']['im'] = imMasked
-
-	fluoCropped = imFluo[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]]
-	fluoMasked = cv.bitwise_and(fluoCropped, fluoCropped, mask = eMask)
-	templates[id]['e']['fluo'] = fluoMasked
-	
-	templates[id]['eSize'] = cv.countNonZero(eMask)
-
-	# cv.imshow('image',eMask)
-	# cv.waitKey(0)
-	# cv.imshow('image',tMask)
-	# cv.waitKey(0)
-	# cv.imshow('image',mMask)
-	# cv.waitKey(0)
-	# cv.imshow('image',imMasked)
-	# cv.waitKey(0)
-	# cv.imshow('image',fluoMasked)
-	# cv.waitKey(0)
-	# cv.destroyAllWindows()
-
-# ######################################
-# # populate nOrientations for each template
-# ######################################
-# angles = np.linspace(start=0, stop=360, num=nAngles)
-# counter = len(templates)
-# for template in templates:
-	# for angle in angles:
-		# template = copy.deepcopy(template)
+		if largerBbox[0]>0 and largerBbox[1]>0 and largerBbox[0]+largerBbox[2]<w and largerBbox[1]+largerBbox[3]<h: # check that the larger bbox is still in the image
+			templates[counter] = {}
 			
-		# xxx a larger bounding box is needed before rotating
-	
-		# templates[counter] = 
-		# counter = counter + 1
+			# rotation matrix
+			M = cv.getRotationMatrix2D((largerBbox[2]/2, largerBbox[3]/2), angle, 1)
+			
+			# rotate the envelope points
+			ePoints = np.array(e['points']) - np.array([largerBbox[0], largerBbox[1]])
+			ePointsRotated = cv.transform(np.array([ePoints]), M)[0]
+			
+			# get new bounding box after rotation
+			bboxAfterRotation = cv.boundingRect(np.array(ePointsRotated))			
+			templates[counter]['bbox'] = bboxAfterRotation
+			
+			# create envelope mask
+			eMask = np.zeros((largerBbox[3], largerBbox[2]), dtype=np.uint8) # envelope mask
+			cv.fillConvexPoly(eMask, ePointsRotated, 255)
+			eMask = eMask[bboxAfterRotation[1]:bboxAfterRotation[1]+bboxAfterRotation[3], bboxAfterRotation[0]:bboxAfterRotation[0]+bboxAfterRotation[2]]
+			
+			# write envelope
+			templates[counter]['e'] = {}
+			templates[counter]['e']['mask'] = eMask
+			templates[counter]['e']['points'] = np.array(ePointsRotated) - np.array([bboxAfterRotation[0], bboxAfterRotation[1]])
+			templates[counter]['eSize'] = cv.countNonZero(eMask)
 
+			# tissue
+			tPoints = np.array(t['points']) - np.array([largerBbox[0], largerBbox[1]])
+			tPointsRotated = cv.transform(np.array([tPoints]), M)[0]
+
+			tMask = np.zeros((largerBbox[3], largerBbox[2]), dtype=np.uint8) # envelope mask
+			cv.fillConvexPoly(tMask, tPointsRotated, 255)
+			tMask = tMask[bboxAfterRotation[1]:bboxAfterRotation[1]+bboxAfterRotation[3], bboxAfterRotation[0]:bboxAfterRotation[0]+bboxAfterRotation[2]]
+			
+			templates[counter]['t'] = {}
+			templates[counter]['t']['points'] = np.array(tPointsRotated) - np.array([bboxAfterRotation[0], bboxAfterRotation[1]])
+			templates[counter]['t']['mask'] = tMask
+
+			# magnet
+			mPoints = np.array(m['points']) - np.array([largerBbox[0], largerBbox[1]])
+			mPointsRotated = cv.transform(np.array([mPoints]), M)[0]
+
+			mMask = np.zeros((largerBbox[3], largerBbox[2]), dtype=np.uint8) # envelope mask
+			cv.fillConvexPoly(mMask, mPointsRotated, 255)
+			mMask = mMask[bboxAfterRotation[1]:bboxAfterRotation[1]+bboxAfterRotation[3], bboxAfterRotation[0]:bboxAfterRotation[0]+bboxAfterRotation[2]]
+			
+			templates[counter]['m'] = {}
+			templates[counter]['m']['points'] = np.array(mPointsRotated) - np.array([bboxAfterRotation[0], bboxAfterRotation[1]])
+			templates[counter]['m']['mask'] = mMask
+
+			# process the images
+			imBoxed = im[largerBbox[1]:largerBbox[1]+largerBbox[3], largerBbox[0]:largerBbox[0]+largerBbox[2]]
+			fluoBoxed = imFluo[largerBbox[1]:largerBbox[1]+largerBbox[3], largerBbox[0]:largerBbox[0]+largerBbox[2]]
+			
+			imRotated = cv.warpAffine(imBoxed, M, (largerBbox[2],largerBbox[3]))			
+			fluoRotated = cv.warpAffine(fluoBoxed, M, (largerBbox[2],largerBbox[3]))
+			
+			imCropped = imRotated[bboxAfterRotation[1]:bboxAfterRotation[1]+bboxAfterRotation[3], bboxAfterRotation[0]:bboxAfterRotation[0]+bboxAfterRotation[2]]
+			imMasked = cv.bitwise_and(imCropped, imCropped, mask = eMask)
+			templates[counter]['e']['im'] = imMasked
+
+			fluoCropped = fluoRotated[bboxAfterRotation[1]:bboxAfterRotation[1]+bboxAfterRotation[3], bboxAfterRotation[0]:bboxAfterRotation[0]+bboxAfterRotation[2]]
+			fluoMasked = cv.bitwise_and(fluoCropped, fluoCropped, mask = eMask)
+			templates[counter]['e']['fluo'] = fluoMasked
+
+			counter = counter+1
+			
+			# cv.imshow('image',eMask)
+			# cv.waitKey(0)
+			# cv.imshow('image',tMask)
+			# cv.waitKey(0)
+			# cv.imshow('image',mMask)
+			# cv.waitKey(0)
+			# cv.imshow('image',imMasked)
+			# cv.waitKey(0)
+			# cv.imshow('image',fluoMasked)
+			# cv.waitKey(0)
+			# cv.destroyAllWindows()
 
 ###############
 # Populate backgrounds and fluoBackgrounds from the labeled background area 
@@ -123,7 +139,7 @@ backgroundBbox = cv.boundingRect(backgroundPoints)
 bx = backgroundBbox[0] + backgroundBbox[2]
 by = backgroundBbox[1] + backgroundBbox[3]
 shiftSize = (basketSize * 0.2).astype(int) # how much does the window slide to generate the background (these are subpatches of the manually selected (large) background area)
-for x in range(10):
+for x in range(10): # no need to bother with correct indices, just try 10x10 boxes, it's enough
 	for y in range(10):
 		x1 = backgroundBbox[0] + x*shiftSize[0]
 		y1 = backgroundBbox[1] + y*shiftSize[1]
